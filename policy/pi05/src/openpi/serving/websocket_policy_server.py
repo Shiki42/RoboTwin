414aacdb1717a780d4c724f8baa93533ccd2fd62
Shiki42 <Shiki42@users.noreply.github.com>
2026-08-10 01:45:35 +0000

    Disable WebSocket keepalive for long policy RPCs

--- policy/pi05/src/openpi/serving/websocket_policy_server.py
+++ policy/pi05/src/openpi/serving/websocket_policy_server.py
@@ -43,6 +43,7 @@ class WebsocketPolicyServer:
             self._port,
             compression=None,
             max_size=None,
+            ping_interval=None,
             process_request=_health_check,
         ) as server:
             await server.serve_forever()
